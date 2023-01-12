61e0150b1d6fb9355b48fcf77ef5a3276200de85
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-12 15:43:26 +0200

    streams

--- src/main/java/lt/Karolis/streams/MainStreams.java
+++ src/main/java/lt/Karolis/streams/MainStreams.java
@@ -1,7 +1,28 @@
 package lt.Karolis.streams;
 
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 public class MainStreams {
     public static void main(String[] args) {
 
+        List<String> list = Arrays.asList("a9", "a2", "b9", "b3", "b5", "b6", "bc8", "ba7");
+        List<String> modList;
+
+        System.out.println(list);
+
+        modList = list.stream().
+                filter(var -> var.contains("a"))
+                .map(var -> var + "_" + UUID.randomUUID())
+                .toList();
+
+        System.out.println(modList);
+
+        list.stream().
+                filter(var -> var.contains("a"))
+                .map(var -> var + "_" + LocalTime.now().getNano())
+                .forEach(System.out::println);
     }
 }
